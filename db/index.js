fee707dcb6d01be6443a518cfad7977915b753d9
Andrew Conlin <andrewpc1013@gmail.com>
2023-03-02 13:58:35 -0600

    added createTags function

--- db/index.js
+++ db/index.js
@@ -164,6 +164,45 @@ async function getPostsByUser(userId) {
   }
 }
 
+async function createTags(tagList) {
+  if (tagList.length === 0) { 
+    return; 
+  }
+
+  // need something like: $1), ($2), ($3 
+  const insertValues = tagList.map(
+    (_, index) => `$${index + 1}`).join('), (');
+  // then we can use: (${ insertValues }) in our string template
+
+  // need something like $1, $2, $3
+  const selectValues = tagList.map(
+    (_, index) => `$${index + 1}`).join(', ');
+  // then we can use (${ selectValues }) in our string template
+
+  try {
+    // insert the tags, doing nothing on conflict
+    // returning nothing, we'll query after
+    await client.query(`
+        INSERT INTO tags(name)
+        VALUES ${insertValues}
+        ON CONFLICT (name) DO NOTHING;
+      `);
+
+    // select all tags where the name is in our taglist
+    // return the rows from the query
+    const result= await client.query(`
+      SELECT * FROM tags
+      WHERE name
+      IN ${selectValues};
+    `);
+
+    return result.rows;
+
+  } catch (error) {
+    throw error;
+  }
+}
+
 module.exports = {
   client,
   getAllUsers,
