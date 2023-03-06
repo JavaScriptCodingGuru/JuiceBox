80eb7dad21aa4027f622c1dea2d064fd683ca9c1
UpcomingWhale <javascriptcodingguru@gmail.com>
2023-03-06 10:37:40 -0500

    added createPost to api

--- api/posts.js
+++ api/posts.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const {getAllPosts} = require('../db')
+const {getAllPosts, createPost} = require('../db')
+const {requireUser} = require('./utils');
 
 const postsRouter = express.Router();
 
@@ -18,6 +19,42 @@ postsRouter.get("/", async(req, res)=>
     res.send({
         posts
     });
+});
+
+postsRouter.post('/', requireUser, async (req, res, next)=>
+{
+    const {title, content, tags=""} = req.body;
+    
+    const tagArr = tags.trim().split(/\s+/);
+    const postData = {};
+
+    if(tagArr.length)
+    {
+        postData.tags = tagArr;
+    }
+
+    try{
+        postData.userId = req.user.id;
+        postData.title = title;
+        postData.content = content;
+
+        const post = await createPost();
+
+        if(post)
+        {
+            res.send({post});
+        }
+        next({
+            name: 'CreatePostError',
+            message: 'Failed to create post'
+        })
+
+    }catch({name, message})
+    {
+        next({name, message});
+    }
+
+    res.send({message:'under construction'});
 });
 
 module.exports = postsRouter;
