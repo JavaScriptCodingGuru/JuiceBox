62ae321d0c4dde69d9966ad48cc965b7e0d0d53a
Andrew Conlin <andrewpc1013@gmail.com>
2023-03-06 09:06:09 -0600

    finished part 2 - part 2: login and register

--- api/users.js
+++ api/users.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {getAllUsers, getUserByUsername} = require('../db')
+const {getAllUsers, getUserByUsername, createUser} = require('../db')
 const jwt = require('jsonwebtoken');
 
 const usersRouter = express.Router();
